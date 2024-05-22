99ccc988711d651868c5818c4a83995723ea9cc1
steffsas <steffen.sassalla@outlook.de>
2024-05-22 06:30:13 +0200

    Add error for DDR bad casting

--- lib/query/ddr.go
+++ lib/query/ddr.go
@@ -8,12 +8,14 @@ func NewDDRQuery() *ConventionalDNSQuery {
 	q := NewConventionalQuery()
 	q.QueryMsg = &dns.Msg{}
 	q.QueryMsg.SetQuestion("_dns.resolver.arpa.", dns.TypeSVCB)
+	q.QueryMsg.RecursionDesired = false
 
 	// see EDNS0: https://datatracker.ietf.org/doc/html/rfc2671
 	// see DNSSEC queries and responses: https://www.ietf.org/rfc/rfc4035
 	// A validating security-aware stub resolver MUST set the DO bit,
 	// because otherwise it will not receive the DNSSEC RRs it needs to
 	// perform signature validation.
+	// TODO consider edns0 for DNSSEC
 	// q.QueryMsg.SetEdns0(4096, true)
 
 	return q
