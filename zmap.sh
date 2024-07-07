e39e5d736fc80105c2e9d3cb87473bb3bc7c7542
steffsas <steffen.sassalla@outlook.de>
2024-07-07 21:07:12 +0200

    Add mkfifo to zmap bash script

--- zmap.sh
+++ zmap.sh
@@ -2,6 +2,11 @@
 # this file is used to scan the entire ipv4 address space for open ports on 53 udp
 # todo: make this more generic, so that it can be used from any directory
 
+tailFile = "/data/zmap/logs/run-$(date +'%Y-%m-%d').log.pipe"
+
+# mkfifo
+mkfifo $tailFile
+
 zmap \
 	-M udp \
    	-p 53 \
@@ -11,4 +16,4 @@ zmap \
    	--probe-args="file:/data/zmap/dns_53_queryAinit.raiun.de.pkt" \
    	-o "/data/ipv4/run-$(date +'%Y-%m-%d').txt" \
    	--verbosity 5 \
-    	0.0.0.0/0 &> "/data/zmap/logs/run-$(date +'%Y-%m-%d').log"
+    	0.0.0.0/0 &> $tailFile
