1cd7078a201600e2aba579aafba0cef936084776
steffsas <steffen.sassalla@outlook.de>
2024-07-07 11:30:48 +0200

    Finalize zmap shell script

--- zmap.sh
+++ zmap.sh
@@ -2,23 +2,11 @@
 # this file is used to scan the entire ipv4 address space for open ports on 53 udp
 # todo: make this more generic, so that it can be used from any directory
 
-# should provide argument interface name
-if [ $# -eq 0 ]; then
-    echo "No arguments provided. Please provide the interface name."
-    exit 1
-fi
-
-# check if the interface exists
-ip addr | grep $1 &> /dev/null
-if [ $? -ne 0 ]; then
-    echo "Interface $1 does not exist."
-    exit 1
-fi
-
 zmap \
--M udp \
+	-M udp \
    	-p 53 \
    	-b "/data/zmap/blocklist.conf" \
+	-G "4a:07:de:5e:0c:1f" \
    	-i $1 \
    	--probe-args="file:/data/zmap/dns_53_queryAinit.raiun.de.pkt" \
    	-o "/data/zmap/results/run-$(date +'%Y-%m-%d').txt" \
