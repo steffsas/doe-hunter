712ff07e0fd73c56b962be782b7a3fb0ceebe46a
Steffen Sassalla <steffen.sassalla@outlook.de>
2024-08-15 18:07:19 +0200

    Fix recursion desired flag to canary probes

--- lib/query/canary.go
+++ lib/query/canary.go
@@ -10,8 +10,7 @@ func NewCanaryQuery(canaryDomain string, host string) *ConventionalDNSQuery {
 	// note that enabling DNSSEC does not make sense here since _dns.resolver.arpa. is a locally persevered zone without any trust anchor
 	// clients have to validate the response by themselves anyway
 	q.QueryMsg.SetQuestion(canaryDomain, dns.TypeA)
-	// this is important since most authoritative servers will not respond to a query with RD set
-	q.QueryMsg.RecursionDesired = false
+	q.QueryMsg.RecursionDesired = true
 
 	// set the host
 	q.Host = host
